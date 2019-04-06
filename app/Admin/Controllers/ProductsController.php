a1b4bb336e25b3a12b8bd8681e896f3856c0534a
vagrant <vagrant@homestead>
2019-04-06 12:40:21 +0000

    add products list

---
+++ app/Admin/Controllers/ProductsController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ProductsController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('产品列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product);
+
+        $grid->id('Id');
+        $grid->class_id('分类 id');
+        $grid->title('产品名称');
+        $grid->created_at('创建时间');
+
+        // 禁用导出
+        $grid->disableExport();
+
+        // 禁用行选择checkbox
+        $grid->disableRowSelector();
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->id('Id');
+        $show->class_id('Class id');
+        $show->title('Title');
+        $show->description('Description');
+        $show->image('Image');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product);
+
+        $form->number('class_id', 'Class id');
+        $form->text('title', 'Title');
+        $form->textarea('description', 'Description');
+        $form->image('image', 'Image');
+
+        return $form;
+    }
+}
